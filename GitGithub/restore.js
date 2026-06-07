db043877fe8c32a40411e1d99d610dee80506ef4
FaizanAhmed2008 <forfaizanahmed@gmail.com>
2026-06-07 14:44:13 +0530

    BackUp repo before trying revent cmd

---
+++ GitGithub/restore.js
@@ -0,0 +1,5 @@
+console.log("Hello, World!");
+
+console.log("This is a simple JavaScript file to demonstrate the restore functionality in GitHub.");
+
+console.log("You can edit this file and then restore it to a previous version using GitHub's interface.");
