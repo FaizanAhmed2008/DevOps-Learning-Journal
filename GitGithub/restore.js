7443018b764ae18d276856d7d9ea7d441882be7a
FaizanAhmed2008 <forfaizanahmed@gmail.com>
2026-06-08 18:35:21 +0530

    Daigram and Branch Concept

--- GitGithub/restore.js
+++ GitGithub/restore.js
@@ -3,3 +3,6 @@ console.log("Hello, World!");
 console.log("This is a simple JavaScript file to demonstrate the restore functionality in GitHub.");
 
 console.log("You can edit this file and then restore it to a previous version using GitHub's interface.");
+
+console.log{"This line is written through serverside branch "};
+
