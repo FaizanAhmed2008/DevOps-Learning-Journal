828a9de39352e6e5e230a76e67a96217633ccd29
FaizanAhmed2008 <forfaizanahmed@gmail.com>
2026-06-09 18:17:27 +0530

    Push without staging area

--- GitGithub/Script/push.py
+++
@@ -1,7 +0,0 @@
-import os
-
-msg = input("Commit : ")
-
-os.system("git add .")
-os.system(f'git commit -m "{msg}"')
-os.system("git push origin main")
--- GitGithub/restore.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello, World!");
-
-console.log("This is a simple JavaScript file to demonstrate the restore functionality in GitHub.");
-
-console.log("You can edit this file and then restore it to a previous version using GitHub's interface.");
-
-console.log("This line is written through serverside branch ");
-
